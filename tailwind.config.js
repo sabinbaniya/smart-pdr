5be8e89d47f5760a27ee01fa3f42dd1811757147
Sabin Baniya <dummymail@tutanota.de>
2022-02-19 17:06:27 +0545

    Nav, hero & companies section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   content: ["./index.html"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        cred: "#FF1727",
+        cgrey: "#717171",
+      },
+    },
+  },
   plugins: [],
 };
