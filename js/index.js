dda0a8d55934ab261476ae376ddb63ee649df8ee
Sabin Baniya <dummymail@tutanota.de>
2022-02-21 09:57:07 +0545

    slideshow our work section

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const overlay = document.querySelector("#overlay");
 overlay.style.display = "none";
 
 function showPopup(heading, text) {
-  console.log("hi");
   overlay.style.display = "block";
   document.querySelector("#popup").innerHTML = `
     <h3 class="h3_style text-black" id="heading">${heading}</h3>
@@ -17,4 +16,47 @@ function showPopup(heading, text) {
 
 function closePopup() {
   overlay.style.display = "none";
+}
+
+// for image slideshow
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  unenlargePicture();
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+function enlargePicture(src) {
+  this.event.stopPropagation();
+  const bigpic = document.querySelector("#bigPicture");
+  const img = document.createElement("img");
+  img.setAttribute("class", "absolute w-screen h-screen");
+  img.setAttribute("id", "enlargedImage");
+  img.setAttribute("src", src);
+  bigpic.appendChild(img);
+}
+
+function unenlargePicture() {
+  const bigpic = document.querySelector("#bigPicture");
+  const enlargedImage = document.querySelector("#enlargedImage");
+  if (enlargedImage) {
+    bigpic.removeChild(enlargedImage);
+  }
 }
